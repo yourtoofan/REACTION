8b5da2f38e90197edbe32bd8f8bb5f6999892dc5
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-31 16:08:40 +0530

    Delete nexichat/modules/optimize_database.py

--- nexichat/modules/optimize_database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from config import MONGO_URL
-
-
-def optimize_database():
-    engine = create_engine(MONGO_URL)
-    with engine.connect() as conn:
-        conn.execute("VACUUM;")
-        conn.execute("ANALYZE;")
