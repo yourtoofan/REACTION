565f1e8e0653b724759f6df43280006051e1def4
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-02 04:11:18 +0530

    Update __chatbot.py

--- nexichat/modules/__chatbot.py
+++ nexichat/modules/__chatbot.py
@@ -76,7 +76,7 @@ async def save_reply(original_message: Message, reply_message: Message):
         is_chat = await chatai.find_one(reply_data)
         if not is_chat:
             await chatai.insert_one(reply_data)
-            await replies_cache.append(reply_data)
+            replies_cache.append(reply_data)
 
     except Exception as e:
         print(f"Error in save_reply: {e}")
@@ -295,9 +295,10 @@ async def save_new_reply(x, new_reply):
         is_chat = await storeai.find_one(reply_data)
         if not is_chat:
             await storeai.insert_one(reply_data)
+            await chatai.delete_one(reply_data)
             new_replies_cache.append(reply_data)
             replies_cache.remove(reply_data)
-            await chatai.delete_one(reply_data)
+            
             
     except Exception as e:
         print(f"Error in save_new_reply: {e}")
