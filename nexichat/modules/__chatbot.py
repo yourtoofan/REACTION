997110689281c6eec2dd41a08d8ab821958febd8
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-02 00:40:38 +0530

    Update __chatbot.py

--- nexichat/modules/__chatbot.py
+++ nexichat/modules/__chatbot.py
@@ -279,7 +279,7 @@ async def refresh_replies_cache():
                     text me message hai uske liye Ekdam chatty aur chhota reply do jitna chhota se chhota reply me kam ho jaye utna hi chota reply do agar jyada bada reply dena ho to maximum 1 line ka dena barna kosis krna chhota sa chhota reply ho aur purane jaise reply mat dena new reply lagna chahiye aur reply mazedar aur simple ho. Jis language mein yeh text hai, usi language mein reply karo. Agar sirf emoji hai toh bas usi se related emoji bhejo. Dhyaan rahe tum ek ladki ho toh reply bhi ladki ke jaise masti bhara ho.
                     Bas reply hi likh ke do, kuch extra nahi aur jitna fast ho sake utna fast reply do!
                     """
-                    response = await api.gemini(user_input)
+                    response = api.gemini(user_input)
                     x = response["results"]
                     
                     if x:
