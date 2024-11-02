1b123da5eed9aa78c78f2ee62a630ef6f60a5d5e
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-03 01:16:28 +0530

    Update __chatbot.py

--- nexichat/modules/__chatbot.py
+++ nexichat/modules/__chatbot.py
@@ -29,7 +29,9 @@ async def get_reply(message_text):
     global replies_cache
     try:
         for reply_data in replies_cache:
+            print("mila")
             if reply_data["word"] == message_text:
+                print("nhi mila")
                 return reply_data["text"], reply_data["check"]
         
         reply_data = await chatai.find_one({"word": message_text})
