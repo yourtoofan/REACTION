48282891a87cea162376c35f94133d150298a982
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-13 01:03:51 +0530

    Update Clone.py

--- nexichat/modules/Clone.py
+++ nexichat/modules/Clone.py
@@ -97,7 +97,7 @@ async def clone_txt(client, message):
             }
 
             await app.send_message(
-                int(OWNER_ID), f"**#New_Clones**\n\n**Bot:- @{bot.username}**\n**Details:-**\n{details}"
+                int(OWNER_ID), f"**#New_Clones**\n\n**Bot:- @{bot.username}**\n**Details:-**\ndetails"
             )
             
             clonebotdb.insert_one(details)
