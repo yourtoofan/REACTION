39933cd487e534d91f8167db0e154f1f704746b1
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-13 09:06:56 +0530

    Update __init__.py

--- nexichat/__init__.py
+++ nexichat/__init__.py
@@ -8,7 +8,24 @@ from pyrogram.enums import ParseMode
 import config
 import uvloop
 import time
+from nexichat import db as mongodb
 
+cloneownerdb = mongodb.cloneownerdb
+clonebotdb = mongodb.clonebotdb
+clonebotnamedb = mongodb.clonebotnamedb
+
+
+async def save_clonebot_owner(bot_id, user_id):
+    await cloneownerdb.insert_one({"bot_id": bot_id, "user_id": user_id})
+
+
+async def get_clonebot_owner(bot_id):
+    result = await cloneownerdb.find_one({"bot_id": bot_id})
+    if result:
+        return result.get("user_id")
+    else:
+        return False
+        
 uvloop.install()
 
 logging.basicConfig(
