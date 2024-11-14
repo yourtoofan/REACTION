a2747993db7b58db4395e586981269b0fd49920c
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-14 15:26:27 +0530

    Create clonestats.py

---
+++ nexichat/database/clonestats.py
@@ -0,0 +1,39 @@
+from nexichat import db as mongodb
+
+cloneownerdb = mongodb.cloneownerdb
+clonebotdb = mongodb.clonebotdb
+
+async def save_clonebot_owner(bot_id, user_id):
+    await cloneownerdb.insert_one({"bot_id": bot_id, "user_id": user_id})
+
+def get_bot_users_collection(bot_id):
+    return mongodb[f"{bot_id}_users"]
+
+def get_bot_chats_collection(bot_id):
+    return mongodb[f"{bot_id}_chats"]
+
+async def is_served_user(bot_id, user_id: int) -> bool:
+    usersdb = get_bot_users_collection(bot_id)
+    return await usersdb.find_one({"user_id": user_id}) is not None
+
+async def add_served_user(bot_id, user_id: int):
+    usersdb = get_bot_users_collection(bot_id)
+    if not await is_served_user(bot_id, user_id):
+        await usersdb.insert_one({"user_id": user_id})
+
+async def get_served_users(bot_id) -> list:
+    usersdb = get_bot_users_collection(bot_id)
+    return await usersdb.find({"user_id": {"$gt": 0}}).to_list(length=None)
+
+async def is_served_chat(bot_id, chat_id: int) -> bool:
+    chatsdb = get_bot_chats_collection(bot_id)
+    return await chatsdb.find_one({"chat_id": chat_id}) is not None
+
+async def add_served_chat(bot_id, chat_id: int):
+    chatsdb = get_bot_chats_collection(bot_id)
+    if not await is_served_chat(bot_id, chat_id):
+        await chatsdb.insert_one({"chat_id": chat_id})
+
+async def get_served_chats(bot_id) -> list:
+    chatsdb = get_bot_chats_collection(bot_id)
+    return await chatsdb.find({"chat_id": {"$lt": 0}}).to_list(length=None)
