afeea76042166428df0eeb9269a7f8efa5076b9e
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-14 20:34:45 +0530

    Create __chatbot.py

---
+++ nexichat/mplugin/__chatbot.py
@@ -0,0 +1,165 @@
+import random
+from pymongo import MongoClient
+from pyrogram import Client, filters
+from pyrogram.errors import MessageEmpty
+from pyrogram.enums import ChatAction, ChatMemberStatus as CMS
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
+from deep_translator import GoogleTranslator
+from nexichat.database.chats import add_served_chat
+from nexichat.database.users import add_served_user
+from config import MONGO_URL
+from nexichat import nexichat, mongo, LOGGER, db
+from nexichat.mplugin.helpers import chatai, CHATBOT_ON, languages
+from nexichat.modules.helpers import (
+    ABOUT_BTN,
+    ABOUT_READ,
+    ADMIN_READ,
+    BACK,
+    CHATBOT_BACK,
+    CHATBOT_READ,
+    DEV_OP,
+    HELP_BTN,
+    HELP_READ,
+    MUSIC_BACK_BTN,
+    SOURCE_READ,
+    START,
+    TOOLS_DATA_READ,
+)
+import asyncio
+
+translator = GoogleTranslator()
+
+lang_db = db.ChatLangDb.LangCollection
+status_db = db.chatbot_status_db.status
+
+replies_cache = []
+
+async def load_replies_cache():
+    global replies_cache
+    replies_cache = await chatai.find().to_list(length=None)
+
+async def save_reply(original_message: Message, reply_message: Message):
+    global replies_cache
+    try:
+        reply_data = {
+            "word": original_message.text,
+            "text": None,
+            "check": "none",
+        }
+
+        if reply_message.sticker:
+            reply_data["text"] = reply_message.sticker.file_id
+            reply_data["check"] = "sticker"
+        elif reply_message.photo:
+            reply_data["text"] = reply_message.photo.file_id
+            reply_data["check"] = "photo"
+        elif reply_message.video:
+            reply_data["text"] = reply_message.video.file_id
+            reply_data["check"] = "video"
+        elif reply_message.audio:
+            reply_data["text"] = reply_message.audio.file_id
+            reply_data["check"] = "audio"
+        elif reply_message.animation:
+            reply_data["text"] = reply_message.animation.file_id
+            reply_data["check"] = "gif"
+        elif reply_message.voice:
+            reply_data["text"] = reply_message.voice.file_id
+            reply_data["check"] = "voice"
+        elif reply_message.text:
+            reply_data["text"] = reply_message.text
+            reply_data["check"] = "none"
+
+        is_chat = await chatai.find_one(reply_data)
+        if not is_chat:
+            await chatai.insert_one(reply_data)
+            replies_cache.append(reply_data)
+
+    except Exception as e:
+        print(f"Error in save_reply: {e}")
+
+async def get_reply(word: str):
+    global replies_cache
+    if not replies_cache:
+        await load_replies_cache()
+        print("Reloaded Chats")
+    relevant_replies = [reply for reply in replies_cache if reply['word'] == word]
+    if not relevant_replies:
+        relevant_replies = replies_cache
+    return random.choice(relevant_replies) if relevant_replies else None
+
+
+
+
+
+
+
+def generate_language_buttons(languages):
+    buttons = []
+    current_row = []
+    for lang, code in languages.items():
+        current_row.append(InlineKeyboardButton(lang.capitalize(), callback_data=f'setlang_{code}'))
+        if len(current_row) == 4:
+            buttons.append(current_row)
+            current_row = []
+    if current_row:
+        buttons.append(current_row)
+    return InlineKeyboardMarkup(buttons)
+
+async def get_chat_language(chat_id):
+    chat_lang = await lang_db.find_one({"chat_id": chat_id})
+    return chat_lang["language"] if chat_lang and "language" in chat_lang else "en"
+    
+
+
+            
+@Client.on_message(filters.incoming)
+async def chatbot_response(client: Client, message: Message):
+    try:
+        chat_id = message.chat.id
+        chat_status = await status_db.find_one({"chat_id": chat_id})
+        
+        if chat_status and chat_status.get("status") == "disabled":
+            return
+
+        if message.text and any(message.text.startswith(prefix) for prefix in ["!", "/", ".", "?", "@", "#"]):
+            if message.chat.type in ["group", "supergroup"]:
+                return await add_served_chat(chat_id)
+            else:
+                return await add_served_user(chat_id)
+        
+        if (message.reply_to_message and message.reply_to_message.from_user.id == client.me.id) or not message.reply_to_message:
+            reply_data = await get_reply(message.text)
+
+            if reply_data:
+                response_text = reply_data["text"]
+                chat_lang = await get_chat_language(chat_id)
+
+                if not chat_lang or chat_lang == "nolang":
+                    translated_text = response_text
+                else:
+                    translated_text = GoogleTranslator(source='auto', target=chat_lang).translate(response_text)
+                
+                if reply_data["check"] == "sticker":
+                    await message.reply_sticker(reply_data["text"])
+                elif reply_data["check"] == "photo":
+                    await message.reply_photo(reply_data["text"])
+                elif reply_data["check"] == "video":
+                    await message.reply_video(reply_data["text"])
+                elif reply_data["check"] == "audio":
+                    await message.reply_audio(reply_data["text"])
+                elif reply_data["check"] == "gif":
+                    await message.reply_animation(reply_data["text"])
+                elif reply_data["check"] == "voice":
+                    await message.reply_voice(reply_data["text"])
+                else:
+                    await message.reply_text(translated_text)
+            else:
+                await message.reply_text("**I don't understand. What are you saying?**")
+
+        if message.reply_to_message:
+            await save_reply(message.reply_to_message, message)
+
+    except MessageEmpty:
+        await message.reply_text("🙄🙄")
+    except Exception as e:
+        return
