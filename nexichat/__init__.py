bd3cd64e3b4a2a6e47787e9e21155c621ba39423
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-14 14:49:59 +0530

    Update __init__.py

--- nexichat/__init__.py
+++ nexichat/__init__.py
@@ -21,7 +21,7 @@ logging.basicConfig(
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 boot = time.time()
-mongodb = MongoCli(config.MONGO_URL)
+mongodb = MongoClient(config.MONGO_URL)
 db = mongodb.VIPMUSIC
 mongo = MongoClient(config.MONGO_URL)
 OWNER = config.OWNER_ID
