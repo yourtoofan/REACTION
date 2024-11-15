b462a4ecad6b7dba5b9e3123504f5d446d24504a
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-16 02:48:17 +0530

    Update fsub.py

--- nexichat/modules/fsub.py
+++ nexichat/modules/fsub.py
@@ -23,7 +23,7 @@ async def must_join_channel(app: Client, msg: Message):
             try:
                 await msg.reply_photo(
                     photo="https://envs.sh/Tn_.jpg",
-                    caption=(f"**👋 ʜᴇʟʟᴏ {message.from_user.mention},**\n\n**ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ [ᴄʜᴀɴɴᴇʟ]({link}) ᴛᴏ sᴇɴᴅ ᴍᴇssᴀɢᴇs ɪɴ ᴛʜɪs ʙᴏᴛ.**"),
+                    caption=(f"**👋 ʜᴇʟʟᴏ {msg.from_user.mention},**\n\n**ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ [ᴄʜᴀɴɴᴇʟ]({link}) ᴛᴏ sᴇɴᴅ ᴍᴇssᴀɢᴇs ɪɴ ᴛʜɪs ʙᴏᴛ.**"),
                     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("๏ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ๏", url=link)]]))
         
                 await msg.stop_propagation()
