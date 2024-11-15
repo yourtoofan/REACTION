320898f5641f731190a0ec0b66658a1f528d9592
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-16 02:01:17 +0530

    Update fsub.py

--- nexichat/modules/fsub.py
+++ nexichat/modules/fsub.py
@@ -6,9 +6,11 @@ from nexichat import nexichat as app, mongo
 import asyncio
 from config import OWNER_ID
 
+# Database connection
 db = mongo['bot_database']
 collection = db['must_join_channel']
 
+# Fetching the force subscription channel from the database
 MUST_JOIN = collection.find_one({"_id": "must_join_channel"})['channel_id'] if collection.find_one({"_id": "must_join_channel"}) else None
 
 @app.on_message(filters.command("setfsub") & filters.user(int(OWNER_ID)))
@@ -18,9 +20,9 @@ async def set_channel(client: Client, message: Message):
     
     channel = message.command[1]
     
-    if channel.startswith('@'): 
+    if channel.startswith('@'):
         link = f"https://t.me/{channel.lstrip('@')}"
-    elif channel.isdigit(): 
+    elif channel.isdigit():
         link = f"https://t.me/c/{channel}"
     else:
         return await message.reply_text("Invalid channel. Please provide a valid channel username or channel ID.")
@@ -28,12 +30,12 @@ async def set_channel(client: Client, message: Message):
     try:
         chat_info = await client.get_chat(channel)
         
-        
+        # Check if the bot is an admin with permission to invite users
         bot_member = await client.get_chat_member(channel, client.me.id)
         if not bot_member.can_invite_users:
             return await message.reply_text(f"Please promote me to admin in {channel} with 'Invite to Group via Link' permissions.")
         
-        
+        # Updating the force subscription channel in the database
         collection.update_one(
             {"_id": "must_join_channel"},
             {"$set": {"channel_id": channel}},
@@ -50,17 +52,21 @@ async def must_join_channel(app: Client, msg: Message):
         try:
             await app.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
+            # Prepare the subscription link
             if MUST_JOIN.isalpha():
                 link = f"https://t.me/{MUST_JOIN}"
             else:
                 chat_info = await app.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
 
+            # Send a message to ask the user to join the required channel
+            photo = "https://envs.sh/Tn_.jpg"
+            caption = (f"**👋 ʜᴇʟʟᴏ {msg.from_user.mention},**\n\n"
+                       f"**ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ [ᴄʜᴀɴɴᴇʟ]({link}) ᴛᴏ sᴇɴᴅ ᴍᴇssᴀɢᴇs ɪɴ ᴛʜɪs ʙᴏᴛ**")
+            reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("๏ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ๏", url=link)]])
+            
             try:
-                photo="https://envs.sh/Tn_.jpg",
-                caption=(f"**👋 ʜᴇʟʟᴏ {message.from_user.mention},**\n\n**ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ [ᴄʜᴀɴɴᴇʟ]({link}) ᴛᴏ sᴇɴᴅ ᴍᴇssᴀɢᴇs ɪɴ ᴛʜɪs ʙᴏᴛ**"),
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("๏ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ๏", url=link)]]))
-        
+                await msg.reply_photo(photo=photo, caption=caption, reply_markup=reply_markup)
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
