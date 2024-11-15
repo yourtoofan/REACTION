cac76103370139e7a9c39c2e9a45ef1e4c2dfc7c
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-16 02:53:45 +0530

    Update and rename fsub.py to Must_join.py

--- nexichat/modules/Must_join.py
+++ nexichat/modules/Must_join.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from nexichat import nexichat as app
-from config import MUST_JOIN
+from config import UPDATE_CHNL as MUST_JOIN
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
