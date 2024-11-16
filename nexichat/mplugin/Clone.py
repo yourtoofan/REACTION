a75e08bfd527de8e89f7a6df4036687aed23967c
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-16 15:48:34 +0530

    Update Clone.py

--- nexichat/mplugin/Clone.py
+++ nexichat/mplugin/Clone.py
@@ -118,7 +118,7 @@ async def delete_cloned_bot(client, message):
         cloned_bot = await clonebotdb.find_one({"token": bot_token})
         if cloned_bot:
             await clonebotdb.delete_one({"token": bot_token})
-            CLONES.remove(cloned_bot["bot_id"])
+            
             await ok.edit_text(
                 "**🤖 your cloned bot has been disconnected from my server ☠️**\n**Clone by :- /clone**"
             )
