a7e9e34428ce6eb74b7202c0c616af157128f77a
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-16 17:29:23 +0530

    Update __chatbot.py

--- nexichat/mplugin/__chatbot.py
+++ nexichat/mplugin/__chatbot.py
@@ -68,12 +68,7 @@ async def save_reply(original_message: Message, reply_message: Message):
             reply_data["check"] = "voice"
         elif reply_message.text:
             translated_text = reply_message.text
-            """try:
-                await asyncio.sleep(1)
-                translated_text = GoogleTranslator(source='auto', target='en').translate(reply_message.text)
-            except Exception as e:
-                print(f"Translation error: {e}, saving original text.")
-                translated_text = reply_message.text"""
+            
             reply_data["text"] = translated_text
             reply_data["check"] = "none"
 
