8b7260cb6c4910129ccfa6fd18973eedfe663f0f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-16 16:53:56 +0530

    Update Clone.py

--- nexichat/mplugin/Clone.py
+++ nexichat/mplugin/Clone.py
@@ -122,7 +122,7 @@ async def delete_cloned_bot(client, message):
             await ok.edit_text(
                 "**🤖 your cloned bot has been disconnected from my server ☠️**\n**Clone by :- /clone**"
             )
-            os.system(f"kill -9 {os.getpid()} && bash start")
+            await client.stop()
         else:
             await message.reply_text("**⚠️ The provided bot token is not in the cloned list.**")
     except Exception as e:
