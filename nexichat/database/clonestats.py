30f0e8b2ba48d157b0bd15a7d1715ad26facb873
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-16 21:32:19 +0530

    Update clonestats.py

--- nexichat/database/clonestats.py
+++ nexichat/database/clonestats.py
@@ -2,11 +2,6 @@ from nexichat import db as mongodb
 
 cloneownerdb = mongodb.cloneownerdb
 clonebotdb = mongodb.clonebotdb
-#chatsdb = mongodb.chats
-#usersdb = mongodb.users
-
-async def save_clonebot_owner(bot_id, user_id):
-    await cloneownerdb.insert_one({"bot_id": bot_id, "user_id": user_id})
 
 def get_bot_users_collection(bot_id):
     from nexichat import db as mongodb
