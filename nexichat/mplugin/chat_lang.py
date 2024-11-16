a82ea070c63c94042ecae5d0002596a8fcd80693
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-16 23:22:42 +0530

    Add files via upload

---
+++ nexichat/mplugin/chat_lang.py
@@ -0,0 +1,52 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from nexichat import nexichat as app, mongo, db
+from MukeshAPI import api
+from nexichat.modules.helpers import chatai, CHATBOT_ON, languages
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
+
+lang_db = db.ChatLangDb.LangCollection
+message_cache = {}
+
+async def get_chat_language(chat_id):
+    chat_lang = await lang_db.find_one({"chat_id": chat_id})
+    return chat_lang["language"] if chat_lang and "language" in chat_lang else None
+
+@app.on_message(filters.text, group=2)
+async def store_messages(client, message: Message):
+    global message_cache
+
+    chat_id = message.chat.id
+    chat_lang = await get_chat_language(chat_id)
+
+    if not chat_lang:
+        if message.from_user and message.from_user.is_bot:
+            return
+
+        if chat_id not in message_cache:
+            message_cache[chat_id] = []
+
+        message_cache[chat_id].append(message)
+
+        if len(message_cache[chat_id]) >= 30:
+            history = "\n\n".join(
+                [f"Text: {msg.text}..." for msg in message_cache[chat_id]]
+            )
+            user_input = f"""
+            sentences list :-
+            [
+            {history}
+            ]
+
+            Above is a list of sentences. Each sentence could be in different languages. Analyze and identify the dominant language used for each sentence. and then Consider the language that appears the most, ignoring any commands like sentence start with /. 
+            Provide only the official language name with language code (like 'en' for English, 'hi' for Hindi). in this format :-
+            Lang Name :- ""
+            Lang code :- ""
+            Do not provide anything else.
+            """
+
+            response = api.gemini(user_input)
+            x = response["results"]
+            reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("sᴇʟᴇᴄᴛ ʟᴀɴɢᴜᴀɢᴇ", callback_data="choose_lang")]])    
+            await message.reply_text(f"**Chat language detected for this chat:**\n\n{history}\n\n**You can set my lang by /lang**", reply_markup=reply_markup)
+            message_cache[chat_id].clear()
