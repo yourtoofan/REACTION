5b03780293d97ea5a92560a389b61df9539c6a35
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-16 22:10:00 +0530

    Update cowner.py

--- nexichat/mplugin/helpers/cowner.py
+++ nexichat/mplugin/helpers/cowner.py
@@ -3,7 +3,7 @@ from config import OWNER_ID
 
 async def is_owner(client, user_id):
     bot_id = client.me.id
-    owner_id = await get_clone_owner(bot_id)
+    owner_id = get_clone_owner(bot_id)
     if owner_id == user_id or user_id == OWNER_ID:
         return True
     return "You don't have permission to use this command on this bot."
