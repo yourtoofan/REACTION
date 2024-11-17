77c274d3d705f500a6697b6e84698a29080cddb1
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-17 19:50:12 +0530

    Create Chatgpt.py

---
+++ nexichat/idchatbot/Chatgpt.py
@@ -0,0 +1,35 @@
+import requests
+from MukeshAPI import api
+from pyrogram import filters, Client
+from pyrogram.enums import ChatAction
+from nexichat import nexichat as app
+
+
+@Client.on_message(filters.command(["gemini", "ai", "ask", "chatgpt"]))
+async def gemini_handler(client, message):
+    if (
+        message.text.startswith(f"/gemini@{client.me.username}")
+        and len(message.text.split(" ", 1)) > 1
+    ):
+        user_input = message.text.split(" ", 1)[1]
+    elif message.reply_to_message and message.reply_to_message.text:
+        user_input = message.reply_to_message.text
+    else:
+        if len(message.command) > 1:
+            user_input = " ".join(message.command[1:])
+        else:
+            await message.reply_text("ᴇxᴀᴍᴘʟᴇ :- `/ask who is Narendra Modi")
+            return
+
+    try:
+        response = api.gemini(user_input)
+        await client.send_chat_action(message.chat.id, ChatAction.TYPING)
+        x = response["results"]
+        if x:
+            await message.reply_text(x, quote=True)
+        else:
+            await message.reply_text("sᴏʀʀʏ sɪʀ! ᴘʟᴇᴀsᴇ Tʀʏ ᴀɢᴀɪɴ")
+    except requests.exceptions.RequestException as e:
+        pass
+
+
