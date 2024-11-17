ccf68460943fda70d545cf2b48e43595605efa34
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-17 18:36:46 +0530

    Create cowner.py

---
+++ nexichat/idchatbot/helpers/cowner.py
@@ -0,0 +1,16 @@
+from nexichat import db
+from config import OWNER_ID
+
+cloneownerdb = db.clone_owners
+
+async def get_clone_owner(bot_id):
+    data = await cloneownerdb.find_one({"bot_id": bot_id})
+    if data:
+        return data["user_id"]
+    return None
+    
+async def is_owner(bot_id, user_id):
+    owner_id = await get_clone_owner(bot_id)
+    if owner_id == user_id or user_id == OWNER_ID:
+        return True
+    return False
