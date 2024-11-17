3c7b1f4ba7cd511900fa113df22bc9683acd7eb1
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-17 14:07:33 +0530

    Update userbot.py

--- nexichat/userbot/userbot.py
+++ nexichat/userbot/userbot.py
@@ -1,17 +1,9 @@
 import asyncio
 from os import getenv
-
+from config import OWNER_ID
 from dotenv import load_dotenv
 from pyrogram import Client
-
-load_dotenv()
-from dotenv import load_dotenv
-
 import config
-
-
-assistants = []
-assistantids = []
 
 
 class Userbot(Client):
@@ -22,12 +14,12 @@ class Userbot(Client):
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=False,
-            plugins=dict(root="VIPMUSIC.plugins.USERBOT"),
+            plugins=dict(root="nexichat.mplugin"),
         )
         
 
     async def start(self):
-        LOGGER(__name__).info(f"Starting Assistants...")
+        print(f"Starting Assistants...")
 
         if config.STRING1:
             await self.one.start()
@@ -36,13 +28,12 @@ class Userbot(Client):
                 await self.one.join_chat("THE_VIP_BOY_OP")
                 await self.one.join_chat("TG_FRIENDSS")
                 await self.one.join_chat("VIP_CREATORS")
-                await self.one.join_chat("TheTeamVivek")
 
             except:
                 pass
-            assistants.append(1)
+            
             try:
-                await self.one.send_message(config.int(OWNER_ID), "ᴀssɪsᴛᴀɴᴛ sᴛᴀʀᴛᴇᴅ !")
+                await self.one.send_message(config.int(OWNER_ID), "**ɪ'ᴍ sᴛᴀʀᴛᴇᴅ ʙᴀʙʏ !**")
                 
             except Exception as e:
                 print(f"{e}")
@@ -50,14 +41,13 @@ class Userbot(Client):
             self.one.id = self.one.me.id
             self.one.name = self.one.me.from 
             self.one.username = self.one.me.username
-            assistantids.append(self.one.id)
-            LOGGER(__name__).info(f"Assistant Started as {self.one.me.first_name}")
+     
+            print(f"Id-Chatbot Started as {self.one.me.first_name}")
 
         
-      
 
     async def stop(self):
-        LOGGER(__name__).info(f"Stopping Assistants...")
+        LOGGER(__name__).info(f"Stopping Id-Chatbot...")
         try:
             if config.STRING1:
                 await self.one.stop()
