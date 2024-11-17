a9d24bb06f569dab24d3b67344d0972b9be42d1e
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-17 11:13:54 +0530

    Update chat_lang.py

--- nexichat/mplugin/chat_lang.py
+++ nexichat/mplugin/chat_lang.py
@@ -56,5 +56,5 @@ async def store_messages(client, message: Message):
 async def fetch_chat_lang(client, message):
     chat_id = message.chat.id
     chat_lang = await get_chat_language(chat_id)
-    await message.reply_text(f"The language using for this chat is: {chat_lang}")
+    await message.reply_text(f"The language code using for this chat is: {chat_lang}")
 
