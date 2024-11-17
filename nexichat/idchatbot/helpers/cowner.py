e9d5a19ce13177dc78c892309c0d6ea38d8d7ea9
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-17 22:22:08 +0530

    Update cowner.py

--- nexichat/idchatbot/helpers/cowner.py
+++ nexichat/idchatbot/helpers/cowner.py
@@ -3,14 +3,21 @@ from config import OWNER_ID
 
 cloneownerdb = db.clone_owners
 
-async def get_clone_owner(bot_id):
-    data = await cloneownerdb.find_one({"bot_id": bot_id})
+async def save_idclonebot_owner(clone_id, user_id):
+    await cloneownerdb.update_one(
+        {"clone_id": clone_id},
+        {"$set": {"user_id": user_id}},
+        upsert=True
+    )
+
+async def get_idclone_owner(clone_id):
+    data = await cloneownerdb.find_one({"clone_id": clone_id})
     if data:
         return data["user_id"]
     return None
     
-async def is_owner(bot_id, user_id):
-    owner_id = await get_clone_owner(bot_id)
+async def is_owner(clone_id, user_id):
+    owner_id = await get_clone_owner(clone_id)
     if owner_id == user_id or user_id == OWNER_ID:
         return True
     return False
