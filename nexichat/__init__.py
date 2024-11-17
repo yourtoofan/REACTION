daa9d7c73d5dc556a606290d1866fa0bb3587e84
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-17 22:20:19 +0530

    Update __init__.py

--- nexichat/__init__.py
+++ nexichat/__init__.py
@@ -59,7 +59,20 @@ async def delete_clone_owner(bot_id):
     await cloneownerdb.delete_one({"bot_id": bot_id})
     CLONE_OWNERS.pop(bot_id, None)
 
+async def save_idclonebot_owner(clone_id, user_id):
+    await cloneownerdb.update_one(
+        {"clone_id": clone_id},
+        {"$set": {"user_id": user_id}},
+        upsert=True
+    )
 
+async def get_idclone_owner(clone_id):
+    data = await cloneownerdb.find_one({"clone_id": clone_id})
+    if data:
+        return data["user_id"]
+    return None
+
+    
 class nexichat(Client):
     def __init__(self):
         super().__init__(
