418a247db3a9375ec8e8302ec76a636c5506b656
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-18 00:58:13 +0530

    Update Id_Clone.py

--- nexichat/modules/Id_Clone.py
+++ nexichat/modules/Id_Clone.py
@@ -66,7 +66,7 @@ async def clone_txt(client, message):
             logging.exception("Error during cloning process.")
             await mi.edit_text(f"**Error:** `{e}`")
     else:
-        await message.reply_text("**Provide a Pyrogram String Session after the /idclone command.**\n\n**Example: /idclone [pyrogram string session]**\n\n**Get a Pyrogram string session from here:-** https://telegram.tools/session-string-generator#pyrogram,user ")
+        await message.reply_text("**Provide a Pyrogram String Session after the /idclone**\n\n**Example: /idclone [pyrogram string session]**\n\n**Get a Pyrogram string session from here:-** [Click Here](https://telegram.tools/session-string-generator#pyrogram,user) ")
 
 
 @app.on_message(filters.command("idcloned"))
