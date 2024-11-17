ca8de68ad25647e1efe0b89915722fbd3ce76317
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-17 12:22:27 +0530

    Update chat_lang.py

--- nexichat/mplugin/chat_lang.py
+++ nexichat/mplugin/chat_lang.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from nexichat import nexichat as app, mongo, db
 from MukeshAPI import api
+import asyncio
 from nexichat.modules.helpers import chatai, CHATBOT_ON, languages
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
 
