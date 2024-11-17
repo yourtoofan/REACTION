8bf6a231a92a72019f6b20265579d8bab307152b
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-17 18:30:43 +0530

    Create storage.py

---
+++ nexichat/idchatbot/helpers/storage.py
@@ -0,0 +1,20 @@
+import random
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+
+CHAT_STORAGE = [
+    "mongodb+srv://chatbot1:a@cluster0.pxbu0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://chatbot2:b@cluster0.9i8as.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://chatbot3:c@cluster0.0ak9k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://chatbot4:d@cluster0.4i428.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://chatbot5:e@cluster0.pmaap.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://chatbot6:f@cluster0.u63li.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://chatbot7:g@cluster0.mhzef.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://chatbot8:h@cluster0.okxao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://chatbot9:i@cluster0.yausb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://chatbot10:j@cluster0.9esnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+]
+
+VIPBOY = MongoCli(random.choice(CHAT_STORAGE))
+chatdb = VIPBOY.Anonymous
+chatai = chatdb.Word.WordDb
+storeai = VIPBOY.Anonymous.Word.NewWordDb  
