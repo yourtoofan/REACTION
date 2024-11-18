9872902153bddf0aba7a6c8757268674e550ad31
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-18 21:26:23 +0530

    Update tagall.py

--- nexichat/idchatbot/tagall.py
+++ nexichat/idchatbot/tagall.py
@@ -21,7 +21,7 @@ async def tag_all_users(client, message):
     replied = message.reply_to_message
     if len(message.command) < 2 and not replied:
         await message.reply_text(
-            "** ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ, ʟɪᴋᴇ »** `@all Hi Friends`"
+            "** ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ, ʟɪᴋᴇ »** `.all Hi Friends`"
         )
         return
     if replied:
