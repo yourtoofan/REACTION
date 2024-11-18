0d88745130bc433c0de367e71a047b2d2ad3b39c
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-18 10:46:54 +0530

    Update Clone.py

--- nexichat/mplugin/Clone.py
+++ nexichat/mplugin/Clone.py
@@ -83,7 +83,7 @@ async def clone_txt(client, message):
                 f"⚠️ <b>Error:</b>\n\n<code>{e}</code>\n\n**Forward this message to @THE_VIP_BOY_OP for assistance**"
             )
     else:
-        await message.reply_text("**Provide Bot Token after /clone Command from @Botfather.**")
+        await message.reply_text("**Provide Bot Token after /clone Command from @Botfather.**\n\n**Example:** `/clone bot token paste here`")
 
 
 @Client.on_message(filters.command("cloned"))
