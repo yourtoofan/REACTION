45397fc1a6603e13ec5a6c88815988fb130018dc
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-18 11:21:36 +0530

    Update Id_Clone.py

--- nexichat/modules/Id_Clone.py
+++ nexichat/modules/Id_Clone.py
@@ -64,9 +64,9 @@ async def clone_txt(client, message):
             await mi.edit_text("**Invalid String Session. Please provide a valid one.**")
         except Exception as e:
             logging.exception("Error during cloning process.")
-            await mi.edit_text(f"**Error:** `{e}`")
+            await mi.edit_text(f"**Invalid String Session. Please provide a valid pyrogram string session.:**\n\n**Error:** `{e}`")
     else:
-        await message.reply_text("**Provide a Pyrogram String Session after the /idclone**\n\n**Example: /idclone [pyrogram string session]**\n\n**Get a Pyrogram string session from here:-** [Click Here](https://telegram.tools/session-string-generator#pyrogram,user) ")
+        await message.reply_text("**Provide a Pyrogram String Session after the /idclone **\n\n**Example:** `/idclone string session paste here`\n\n**Get a Pyrogram string session from here:-** [Click Here](https://telegram.tools/session-string-generator#pyrogram,user) ")
 
 
 @app.on_message(filters.command("idcloned"))
@@ -97,7 +97,7 @@ async def list_cloned_sessions(client, message):
 async def delete_cloned_session(client, message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("**⚠️ Please provide the session string after the command.**")
+            await message.reply_text("**⚠️ Please provide the string session after the command.**\n\n**Example:** `/delidclone your string session here`")
             return
 
         string_session = " ".join(message.command[1:])
