68dfa0fa926deeea15d64d2423d87a6c67d17116
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-19 16:59:55 +0530

    Update update.py

--- nexichat/userbot/update.py
+++ nexichat/userbot/update.py
@@ -1,66 +1,14 @@
-import asyncio
-import shlex
-from typing import Tuple
-from git import Repo
-from git.exc import GitCommandError, InvalidGitRepositoryError
-import config
+import subprocess
 
-loop = asyncio.get_event_loop_policy().get_event_loop()
-
-def install_req(cmd: str) -> Tuple[str, str, int, int]:
-    async def install_requirements():
-        args = shlex.split(cmd)
-        process = await asyncio.create_subprocess_exec(
-            *args,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-        stdout, stderr = await process.communicate()
-        return (
-            stdout.decode("utf-8", "replace").strip(),
-            stderr.decode("utf-8", "replace").strip(),
-            process.returncode,
-            process.pid,
-        )
-
-    return loop.run_until_complete(install_requirements())
-
-def git():
-    REPO_LINK = config.UPSTREAM_REPO
-    if config.GIT_TOKEN:
-        GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
-        TEMP_REPO = REPO_LINK.split("https://")[1]
-        UPSTREAM_REPO = f"https://{GIT_USERNAME}:{config.GIT_TOKEN}@{TEMP_REPO}"
-    else:
-        UPSTREAM_REPO = config.UPSTREAM_REPO
-    
+def install_requirements():
     try:
-        repo = Repo("/app")
-        print(f"Git Client Found [VPS DEPLOYER]")
-    except GitCommandError:
-        print(f"Invalid Git Command")
-    except InvalidGitRepositoryError:
-        repo = Repo.init("/app")
-        if "origin" in repo.remotes:
-            origin = repo.remote("origin")
-        else:
-            origin = repo.create_remote("origin", UPSTREAM_REPO)
-        origin.fetch()
-        repo.create_head(config.UPSTREAM_BRANCH, origin.refs[config.UPSTREAM_BRANCH])
-        repo.heads[config.UPSTREAM_BRANCH].set_tracking_branch(origin.refs[config.UPSTREAM_BRANCH])
-        repo.heads[config.UPSTREAM_BRANCH].checkout(True)
+        subprocess.run(["pip3", "install", "--no-cache-dir", "-r", "requirements.txt"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing requirements: {e}")
 
-        try:
-            repo.create_remote("origin", config.UPSTREAM_REPO)
-        except BaseException:
-            pass
-
-    nrs = repo.remote("origin")
-    nrs.fetch(config.UPSTREAM_BRANCH)
+def update():
     try:
-        nrs.pull(config.UPSTREAM_BRANCH)
-    except GitCommandError:
-        repo.git.reset("--hard", "FETCH_HEAD")
-
-    install_req("pip3 install --no-cache-dir -r requirements.txt")
-    print(f"Fetched Updates from: {UPSTREAM_REPO}")
+        subprocess.run(["git", "pull", "origin", "main"], check=True)
+        install_requirements()
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred during the update process: {e}")
