f219381be876b4827ffca4dcb5652a0b4608a88f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-20 02:51:19 +0530

    Update storage.py

--- nexichat/modules/helpers/storage.py
+++ nexichat/modules/helpers/storage.py
@@ -17,5 +17,4 @@ CHAT_STORAGE = [
 VIPBOY = MongoCli(random.choice(CHAT_STORAGE))
 chatdb = VIPBOY.Anonymous
 chatai = chatdb.Word.WordDb
-abuse_words_db = chatdb.abuse_words_db.words
 storeai = VIPBOY.Anonymous.Word.NewWordDb  
