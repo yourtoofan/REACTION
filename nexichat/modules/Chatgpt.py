41055b356233c0593a0d48de986a326700936371
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-19 10:21:34 +0530

    Update Chatgpt.py

--- nexichat/modules/Chatgpt.py
+++ nexichat/modules/Chatgpt.py
@@ -5,10 +5,10 @@ from pyrogram.enums import ChatAction
 from nexichat import nexichat as app
 
 
-@app.on_message(filters.command(["gemini", "ai", "ask", "chatgpt"]))
+@Client.on_message(filters.command(["gemini", "ai", "ask", "chatgpt"]))
 async def gemini_handler(client, message):
     if (
-        message.text.startswith(f"/gemini@{app.username}")
+        message.text.startswith(f"/gemini@{client.me.username}")
         and len(message.text.split(" ", 1)) > 1
     ):
         user_input = message.text.split(" ", 1)[1]
@@ -18,18 +18,25 @@ async def gemini_handler(client, message):
         if len(message.command) > 1:
             user_input = " ".join(message.command[1:])
         else:
-            await message.reply_text("ᴇxᴀᴍᴘʟᴇ :- `/ask who is Narendra Modi")
+            await message.reply_text("ᴇxᴀᴍᴘʟᴇ :- `/ask who is Narendra Modi`")
             return
 
     try:
         response = api.gemini(user_input)
         await client.send_chat_action(message.chat.id, ChatAction.TYPING)
-        x = response["results"]
-        if x:
-            await message.reply_text(x, quote=True)
+        result = response.get("results")
+        if result:
+            await message.reply_text(result, quote=True)
+            return
+    except:
+        pass  
+        
+    try:
+        base_url = "https://chatwithai.codesearch.workers.dev/?chat="
+        response = requests.get(base_url + user_input)
+        if response and response.text.strip():
+            await message.reply_text(response.text.strip(), quote=True)
         else:
-            await message.reply_text("sᴏʀʀʏ sɪʀ! ᴘʟᴇᴀsᴇ Tʀʏ ᴀɢᴀɪɴ")
-    except requests.exceptions.RequestException as e:
-        pass
-
-
+            await message.reply_text("**Both Gemini and Chat with AI are currently unavailable**")
+    except:
+        await message.reply_text("**Chatgpt is currently dead. Try again later.**")
