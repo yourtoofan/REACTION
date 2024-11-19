0561515cfb08cab5a46bc76122f6da6b462e9c26
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-19 10:16:17 +0530

    Update Chatgpt.py

--- nexichat/idchatbot/Chatgpt.py
+++ nexichat/idchatbot/Chatgpt.py
@@ -5,7 +5,7 @@ from pyrogram.enums import ChatAction
 from nexichat import nexichat as app
 
 
-@Client.on_message(filters.command(["gemini", "ai", "ask", "chatgpt"], prefixes=[".", "/"]))
+@Client.on_message(filters.command(["gemini", "ai", "ask", "chatgpt"]))
 async def gemini_handler(client, message):
     if (
         message.text.startswith(f"/gemini@{client.me.username}")
@@ -18,18 +18,25 @@ async def gemini_handler(client, message):
         if len(message.command) > 1:
             user_input = " ".join(message.command[1:])
         else:
-            await message.reply_text("ᴇxᴀᴍᴘʟᴇ :- `/ask who is Narendra Modi")
+            await message.reply_text("ᴇxᴀᴍᴘʟᴇ :- `/ask who is Narendra Modi`")
             return
 
     try:
         response = api.gemini(user_input)
         await client.send_chat_action(message.chat.id, ChatAction.TYPING)
-        x = response["results"]
-        if x:
-            await message.reply_text(x, quote=True)
+        result = response.get("results")
+        if result:
+            await message.reply_text(result, quote=True)
+            return
+    except:
+        pass  
+        
+    try:
+        base_url = "https://chatwithai.codesearch.workers.dev/?chat="
+        response = requests.get(base_url + user_input)
+        if response and response.text.strip():
+            await message.reply_text(response.text.strip(), quote=True)
         else:
-            await message.reply_text("sᴏʀʀʏ sɪʀ! ᴘʟᴇᴀsᴇ Tʀʏ ᴀɢᴀɪɴ")
-    except requests.exceptions.RequestException as e:
-        pass
-
-
+            await message.reply_text("**Both Gemini and Chat with AI are currently unavailable**")
+    except:
+        await message.reply_text("**Chatgpt is currently dead. Try again later.**")
