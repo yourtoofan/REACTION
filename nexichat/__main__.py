dbe92426b64d2623f183172b285f1ce047af1831
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-20 20:31:33 +0530

    Update __main__.py

--- nexichat/__main__.py
+++ nexichat/__main__.py
@@ -4,14 +4,13 @@ import importlib
 from flask import Flask
 import threading
 import config
-from nexichat import ID_CHATBOT
 from pyrogram import idle
 from pyrogram.types import BotCommand
 from config import OWNER_ID
-from nexichat import LOGGER, nexichat, userbot, load_clone_owners
+from nexichat import LOGGER, nexichat, load_clone_owners
 from nexichat.modules import ALL_MODULES
 from nexichat.modules.Clone import restart_bots
-from nexichat.modules.Id_Clone import restart_idchatbots
+
 
 async def anony_boot():
     try:
@@ -22,19 +21,9 @@ async def anony_boot():
             LOGGER.info(f"@{nexichat.username} Started, please start the bot from owner id.")
     
         asyncio.create_task(restart_bots())
-        asyncio.create_task(restart_idchatbots())
+        
         await load_clone_owners()
-        if config.STRING1:
-            try:
-                await userbot.start()
-                try:
-                    await nexichat.send_message(int(OWNER_ID), f"**Id-Chatbot Also Started✅**")
-                except Exception as ex:
-                    LOGGER.info(f"@{nexichat.username} Started, please start the bot from owner id.")
-    
-            except Exception as ex:
-                print(f"Error in starting id-chatbot :- {ex}")
-                pass
+        
     except Exception as ex:
         LOGGER.error(ex)
 
@@ -49,19 +38,11 @@ async def anony_boot():
                 BotCommand("start", "Start the bot"),
                 BotCommand("help", "Get the help menu"),
                 BotCommand("clone", "Make your own chatbot"),
-                BotCommand("idclone", "Make your id-chatbot"),
                 BotCommand("cloned", "Get List of all cloned bot"),
                 BotCommand("ping", "Check if the bot is alive or dead"),
-                BotCommand("lang", "Select bot reply language"),
-                BotCommand("chatlang", "Get current using lang for chat"),
-                BotCommand("resetlang", "Reset to default bot reply lang"),
                 BotCommand("id", "Get users user_id"),
                 BotCommand("stats", "Check bot stats"),
                 BotCommand("gcast", "Broadcast any message to groups/users"),
-                BotCommand("chatbot", "Enable or disable chatbot"),
-                BotCommand("status", "Check chatbot enable or disable in chat"),
-                BotCommand("shayri", "Get random shayri for love"),
-                BotCommand("ask", "Ask anything from chatgpt"),
                 BotCommand("repo", "Get chatbot source code"),
             ]
         )
