a6d05a110f59227ca53b78cf3ce502ae0854b892
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-20 10:35:09 +0530

    Update Start.py

--- nexichat/modules/Start.py
+++ nexichat/modules/Start.py
@@ -302,7 +302,7 @@ async def start(_, m: Message):
         chats = len(await get_served_chats())
         UP, CPU, RAM, DISK = await bot_sys_stats()
         await m.reply_photo(photo=chat_photo, caption=START.format(nexichat.mention or "can't mention", users, chats, UP), reply_markup=InlineKeyboardMarkup(START_BOT))
-        await message.reply_text(f"**{AUTO_MSG}**")
+        await m.reply_text(f"**{AUTO_MSG}**")
         await add_served_user(m.chat.id)
         keyboard = InlineKeyboardMarkup([[InlineKeyboardButton(f"{m.chat.first_name}", user_id=m.chat.id)]])
         await nexichat.send_photo(int(OWNER_ID), photo=chat_photo, caption=f"{m.from_user.mention} ʜᴀs sᴛᴀʀᴛᴇᴅ ʙᴏᴛ. \n\n**ɴᴀᴍᴇ :** {m.chat.first_name}\n**ᴜsᴇʀɴᴀᴍᴇ :** @{m.chat.username}\n**ɪᴅ :** {m.chat.id}\n\n**ᴛᴏᴛᴀʟ ᴜsᴇʀs :** {users}", reply_markup=keyboard)
@@ -313,7 +313,7 @@ async def start(_, m: Message):
             caption=GSTART.format(m.from_user.mention or "can't mention"),
             reply_markup=InlineKeyboardMarkup(HELP_START),
         )
-        await message.reply_text(f"**{AUTO_MSG}**")
+        await m.reply_text(f"**{AUTO_MSG}**")
         await add_served_chat(m.chat.id)
 
 
