26157a201f4fb04bacef9d6668442747f9fa8529
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-20 13:43:23 +0530

    Update cowner.py

--- nexichat/mplugin/helpers/cowner.py
+++ nexichat/mplugin/helpers/cowner.py
@@ -1,4 +1,4 @@
-from nexichat import db
+from nexichat import db, SUDOERS
 from config import OWNER_ID
 
 cloneownerdb = db.clone_owners
@@ -11,6 +11,6 @@ async def get_clone_owner(bot_id):
     
 async def is_owner(bot_id, user_id):
     owner_id = await get_clone_owner(bot_id)
-    if owner_id == user_id or user_id == OWNER_ID:
+    if owner_id == user_id or user_id == OWNER_ID or user_id in SUDOERS:
         return True
     return False
