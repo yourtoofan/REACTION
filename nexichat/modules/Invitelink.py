978a860f71044b2e4741b5a3a1374b9381c66bad
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-20 15:54:05 +0530

    Update Invitelink.py

--- nexichat/modules/Invitelink.py
+++ nexichat/modules/Invitelink.py
@@ -4,18 +4,18 @@ from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from pyrogram.types import Message
 
-from nexichat import nexichat
+from nexichat import nexichat, SUDOERS
 from config import OWNER_ID
 
 
-@nexichat.on_message(filters.command("givelink") & filters.user(int(OWNER_ID)))
+@nexichat.on_message(filters.command("givelink") & SUDOERS)
 async def give_link_command(client, message):
     chat = message.chat.id
     link = await client.export_chat_invite_link(chat)
     await message.reply_text(f"**Here's the invite link for this chat:**\n\n{link}")
 
 
-@nexichat.on_message(filters.command(["link", "invitelink"], prefixes=["/", "!", "%", ",", ".", "@", "#"]) & filters.user(int(OWNER_ID)))
+@nexichat.on_message(filters.command(["link", "invitelink"], prefixes=["/", "!", "%", ",", ".", "@", "#"]) & SUDOERS)
 async def link_command_handler(client: Client, message: Message):
     if len(message.command) != 2:
         await message.reply("**Invalid usage. Correct format: /link group_id**")
