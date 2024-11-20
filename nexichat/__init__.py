cbc838bb69b908f9c87cbed4a2ed51fc0be3f33e
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-20 14:19:38 +0530

    Update __init__.py

--- nexichat/__init__.py
+++ nexichat/__init__.py
@@ -41,21 +41,19 @@ def sudo():
     global SUDOERS
     OWNER = config.OWNER_ID
     if config.MONGO_URL is None:
-        for user_id in OWNER:
-            SUDOERS.add(user_id)
+        SUDOERS.add(OWNER)
     else:
         sudoersdb = mongodb.sudoers
         sudoers = sudoersdb.find_one({"sudo": "sudo"})
         sudoers = [] if not sudoers else sudoers["sudoers"]
-        for user_id == OWNER:
-            SUDOERS.add(user_id)
-            if user_id not in sudoers:
-                sudoers.append(user_id)
-                sudoersdb.update_one(
-                    {"sudo": "sudo"},
-                    {"$set": {"sudoers": sudoers}},
-                    upsert=True,
-                )
+        SUDOERS.add(OWNER)
+        if OWNER not in sudoers:
+            sudoers.append(OWNER)
+            sudoersdb.update_one(
+                {"sudo": "sudo"},
+                {"$set": {"sudoers": sudoers}},
+                upsert=True,
+            )
         if sudoers:
             for x in sudoers:
                 SUDOERS.add(x)
