44cdc4c217d278e1c7736e233e7d9f989db8d35f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-20 14:55:54 +0530

    Update sudoers.py

--- nexichat/database/sudoers.py
+++ nexichat/database/sudoers.py
@@ -13,7 +13,7 @@ async def get_sudoers() -> list:
 async def add_sudo(user_id: int) -> bool:
     sudoers = await get_sudoers()
     sudoers.append(user_id)
-    await sudoersdb.update_one(
+    sudoersdb.update_one(
         {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
     )
     return True
