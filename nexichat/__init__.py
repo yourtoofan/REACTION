fa7fd57f0729cafead1efd64db47f614c766f36a
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-20 13:56:46 +0530

    Update __init__.py

--- nexichat/__init__.py
+++ nexichat/__init__.py
@@ -39,7 +39,7 @@ def dbb():
     
 def sudo():
     global SUDOERS
-    OWNER = int(config.OWNER_ID)
+    OWNER = config.OWNER_ID
     if config.MONGO_URL is None:
         for user_id in OWNER:
             SUDOERS.add(user_id)
