35746791a2dd29396fa6d0649520539b71398c72
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-20 20:52:51 +0530

    Update Reaction.py

--- nexichat/mplugin/Reaction.py
+++ nexichat/mplugin/Reaction.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 
-@Client.on_message(filters.incoming)
+@Client.on_message(filters.incoming, group=5)
 async def react_to_messages(client: Client, message: Message):
     try:
         await client.react(message.chat.id, message.id, "👍")
