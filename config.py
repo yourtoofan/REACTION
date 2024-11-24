9f24a5ef468c44a2eb0ae6547052bf7f00f76326
Legend mickey <toofanpapa424@gmail.com>
2024-11-24 22:15:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
+OWNER_ID = int(getenv("OWNER_ID", "7520092354"))
 SUPPORT_GRP = "THE_INCRICIBLE"
 UPDATE_CHNL = "THE_INCRICIBLE"
 OWNER_USERNAME = "LEGEND_MICKEY"
